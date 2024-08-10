5a4a9ac625915742a0d5f4cc40be446751fdb0e6
hanswryy <farhan.hans257@gmail.com>
2024-08-10 19:59:12 +0700

    react app added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
 
